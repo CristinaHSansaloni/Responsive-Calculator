ff6ff24b5413cbaa40cf76a278b0acf51e5cb233
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-10-09 04:23:58 +0200

    styles and functions

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+let outputScreen = document.querySelector('.output_screen');
+
+function display(num) {
+    outputScreen.value += num;
+}
+
+function Calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid")
+    }
+}
+
+function Clear() {
+    outputScreen.value = "";
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
